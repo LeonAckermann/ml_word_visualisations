69c7274c023c3f10853fccade73a7eac9f02974f
LeonAckermann <leonmax.ackermann@gmail.com>
2023-11-10 16:21:35 +0100

    added topic naming and p threshold for wordclouds

--- lda/wordclouds.R
+++ lda/wordclouds.R
@@ -36,12 +36,15 @@ create_plots <- function(df_list,
                          cor_var,
                          color_negative_cor,
                          color_positive_cor,
+                         plot_topics_idx,
+                         p_threshold,
                          save_dir="."){
-  for (i in 1:length(df_list)){
+  for (i in plot_topics_idx){
+  #for (i in 1:length(df_list)){
     #view(df_list[[i]])
     if (test_type == "linear_regression"){
       estimate_col <- paste0(cor_var,".estimate") # grep(partial_name, data_frame_names, value = TRUE)
-      p_adjusted <- paste0(cor_var,".p_adjusted")
+      p_adjusted_col <- paste0(cor_var,".p_adjusted")
     } else if (test_type == "t-test"){
       estimate_col <- "cohens d" # probably doesnt work yet
     } else if (test_type == "logistic_regression"){
@@ -49,30 +52,37 @@ create_plots <- function(df_list,
       estimate_col <- "p_adjustedfdr"
     }
     estimate <- test[i,][[estimate_col]]# $PHQtot.estimate
-    p_adjusted <- test[i,][[estimate_col]] # $PHQtot.p_adjustedfdr
-    #estimate <- test[i,][[grep(estimate_col, colnames(test), value=TRUE)]]# $PHQtot.estimate
-    #p_adjusted <- test[i,][[grep("p_adjusted", colnames(test), value=TRUE)]] # $PHQtot.p_adjustedfdr
-    if (estimate < 0){
-      color_scheme <- color_negative_cor # scale_color_gradient(low = "darkgreen", high = "green")
-    } else {
-      color_scheme <- color_positive_cor # scale_color_gradient(low = "darkred", high = "red")
-    }
-    plot <- ggplot(df_list[[i]], aes(label = Word, size = phi, color = phi)) +
-      geom_text_wordcloud() +
-      scale_size_area(max_size = 10) +
-      theme_minimal() +
-      color_scheme
-    if (!dir.exists(save_dir)) {
-        # Create the directory
-      dir.create(save_dir)
-      cat("Directory created successfully.\n")
-    } 
-    if(!dir.exists(paste0(save_dir, "/seed_", seed, "/wordclouds"))){
-      dir.create(paste0(save_dir, "/seed_", seed, "/wordclouds"))
-    }
-    ggsave(paste0(save_dir,"/seed_", seed, "/wordclouds/t_", i, "_r_", estimate, "_p_", p_adjusted,".png"), plot = plot, width = 10, height = 8, units = "in")
+    p_adjusted <- test[i,][[p_adjusted_col]] # $PHQtot.p_adjustedfdr
+    print(i)
+    print(p_adjusted)
+    
+    #if (grep(paste0(i, plo)))
+    if (p_adjusted < p_threshold){
       
-
+      
+      #estimate <- test[i,][[grep(estimate_col, colnames(test), value=TRUE)]]# $PHQtot.estimate
+      #p_adjusted <- test[i,][[grep("p_adjusted", colnames(test), value=TRUE)]] # $PHQtot.p_adjustedfdr
+      if (estimate < threshold){
+        color_scheme <- color_negative_cor # scale_color_gradient(low = "darkgreen", high = "green")
+      } else {
+        color_scheme <- color_positive_cor # scale_color_gradient(low = "darkred", high = "red")
+      }
+      plot <- ggplot(df_list[[i]], aes(label = Word, size = phi, color = phi)) +
+        geom_text_wordcloud() +
+        scale_size_area(max_size = 10) +
+        theme_minimal() +
+        color_scheme
+      if (!dir.exists(save_dir)) {
+          # Create the directory
+        dir.create(save_dir)
+        cat("Directory created successfully.\n")
+      } 
+      if(!dir.exists(paste0(save_dir, "/seed_", seed, "/wordclouds"))){
+        dir.create(paste0(save_dir, "/seed_", seed, "/wordclouds"))
+      }
+      p_adjusted <- sprintf("%.2e", p_adjusted)
+      ggsave(paste0(save_dir,"/seed_", seed, "/wordclouds/t_", i, "_r_", estimate, "_p_", p_adjusted,".png"), plot = plot, width = 10, height = 8, units = "in")
+    }
   }
 }
 
@@ -83,6 +93,8 @@ plot_wordclouds <- function(model,
                             cor_var,
                             color_negative_cor,
                             color_positive_cor,
+                            plot_topics_idx,
+                            p_threshold,
                             save_dir,
                             seed){
   df_list <- create_topic_words_dfs(model$summary)
@@ -93,6 +105,8 @@ plot_wordclouds <- function(model,
                cor_var=cor_var,
                color_negative_cor = color_negative_cor,
                color_positive_cor = color_positive_cor,
+               plot_topics_idx=plot_topics_idx,
+               p_threshold=p_threshold,
                save_dir=save_dir)
   print(paste0("The plots are saved in ", save_dir, "/seed", seed, "/wordclouds"))
 }

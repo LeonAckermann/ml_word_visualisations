30f34b03fc2aecb508073df26ff463b8d3517ceb
moomoofarm1 <46774289+moomoofarm1@users.noreply.github.com>
2023-11-24 18:56:14 +0100

    Update bert_topic.py

--- topic_modeling/bert_topic/bert_topic.py
+++ topic_modeling/bert_topic/bert_topic.py
@@ -20,6 +20,7 @@ import os
 embedding_models = {"miniLM":SentenceTransformer("all-MiniLM-L6-v2"),
                     #"roberta": TransformerDocumentEmbeddings('roberta-base'),
                     "roberta-large": TransformerDocumentEmbeddings('roberta-large'),
+                    "multilingual-e5-large": SentenceTransformer("intfloat/multilingual-e5-large"),
                     "mpnet": SentenceTransformer("all-mpnet-base-v2"),
                     "multi-mpnet": SentenceTransformer("multi-qa-mpnet-base-dot-v1"),
                     "distilroberta": SentenceTransformer("all-distilroberta-v1")}
